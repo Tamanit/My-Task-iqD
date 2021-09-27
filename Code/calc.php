c0bce838323d7a9ca2d54997009f65ad039bd4db
Tamanit <a.plokhikh.sas@gmail.com>
2021-09-27 12:45:46 +0500

    fully complete

--- Code/calc.php
+++ Code/calc.php
@@ -1,84 +1,91 @@
-<? php
-
-function fValidateDate ($Variable, $VariablePOST )
+<?php
+ 
+function fValidateDate ($Variable, $ErrorNum )
 {//Валидация и инициализация даты
 
-	$Variable = explode(".", $VariablePOST);
+	$Variable = explode(".", $Variable);
 
-	if ( $Variable[0] < 1 || $Variable[0] > 31 )
+	if ( (int)$Variable[0] < 1 || (int)$Variable[0] > 31 )
 	{
-		$Variable = -404;
-		echo json_encode ( $Variable);
-	}
-	if ( $Variable[1] < 1 || $Variable[0] > 12 )
+		(int)$Variable[0] = $ErrorNum;
+		exit ( json_encode($Variable[0]));
+	};
+	if ( (int)$Variable[1] < 1 || (int)$Variable[0] > 12 )
 	{
-		$Variable = -404;
-		echo json_encode ( $Variable);
-	}
-	if ( $Variable[2] < 1 || $Variable[2] > 9999 )
+		$Variable[1] = $ErrorNum;
+		exit (json_encode( $Variable[1]));
+	};
+	if ( (int)$Variable[2] < 1 || (int)$Variable[2] > 9999 )
 	{
-		$Variable = -404;
-		echo json_encode ( $Variable);
+		$Variable[2] = $ErrorNum;
+		exit ( json_encode($Variable[2]));
 	}
 	return $Variable;
-}
+};
 
-function fValidateData ($minValue, $maxValue, $Variable, $VariablePOST )
+function fValidateData ($minValue, $maxValue, $Variable, $ErrorNum)
 {//Валидация и инициализация данных
-	if ( $Variable < $minValue || $Variable > $maxValue )
+	if ( (int)$Variable < (int)$minValue || (int)$Variable > (int)$maxValue )
 	{
-		$Variable = -404;
-		echo json_encode ( $Variable);
+		$Variable = $ErrorNum;
+		exit ( json_encode($Variable));
 	}
 	else
 	{
 		$Variable = $VariablePOST;
-	}
+	};
 	return $Variable;
-}
-
-function fCalculation ($sum, $sumAdd, $DaysY, $percent, $yearD, $startDate, $DaysMMod4 )
-{// формула
-		$sum = $sum + ( $sum + $sumAdd ) * $DaysY * (percent/YearD);
-	$startDate[1]++;
-	if ($startDate[1] = 13)
-	{
-		$startDate[1] = 1;
-		$startDate[2]++;
-	}
-	$DaysY = $DaysMMod4[$startDate[1] - 1];
-}
+};
 
 				//		0    1   2   3   4   5   6   7   8   9  10  11
 $DaysMMod4True = Array( 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );	
 $DaysMMod4False = Array( 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
 
-//инициализация
-$startDate = fValidateDate($startDate, $_POST["startDate"]);
-$sum = fValidateData (1000, 3000000, $sum, $_POST["sum"] );
-$term = fValidateData (1, 60, $term, $_POST["term"] );
-$percent = fValidateData (3, 100, percent, $_POST["percent"] );
-$sumAdd = fValidateData (0, 3000000, $sumAdd, $_POST["sumAdd"] );
+$startDate = $_POST["startDate"]; 
+	fValidateDate ($startDate, "NotValidateData(startDate)"); 
+$sum = $_POST["sum"];
+	fValidateData (1000, 3000000, $sum, "NotValidateData(sum)"); 
+$term = $_POST["term"];
+	fValidateData (1, 60, $term, "NotValidateData(term)"); 
+$percent = $_POST["percent"];
+	fValidateData (3, 100, $percent, "NotValidateData(percent)"); 
+$sumAdd = $_POST["sumAdd"];
+	fValidateData (0, 3000000, $sumAdd, "NotValidateData(sumAdd)"); 
 if ($startDate % 4 == 0)
 { //DaysY инициализация
-	$DaysY = $DaysMMod4True[$startDate[1]-1] - $DaysMMod4True[0];
+	(int)$DaysY = (int)$DaysMMod4True[(int)$startDate[1]-1] - (int)$DaysMMod4True[0];
 }
 else
 {
-	$DaysY = $DaysMMod4False[$startDate[1]-1] - $DaysMMod4False[0];
-}
+	(int)$DaysY = (int)$DaysMMod4False[(int)$startDate[1]-1] - (int)$DaysMMod4False[0];
+};
 
-for ($term; $term > 0; $term-- )
+for ($i = (int)$term; $i > 0; $i-- )
 {
-	if ($startDate[2] % 4 == 0)
+	if ((int)$startDate[2] % 4 == 0)
 	{
-		fCalculation ($sum, $sumAdd, $DaysY, $percent, 366, $startDate, $DaysMMod4True );
+		$sum = (real)$sum + ((int)$sum + (int)$sumAdd) *  (((int)$DaysY * (int)$percent) / 365);
+		(int)$startDate[1] = (int)$startDate[1] + 1;
+		if ((int)$startDate[1] == 13)
+		{
+			(int)$startDate[1] = 1;
+			(int)$startDate[2]++;
+		};
+		(int)$DaysY = (int)$DaysMMod4True[(int)$startDate[1] - 1];
+		
 	}
 	else
 	{
-		fCalculation ($sum, $sumAdd, $DaysY, $percent, 365, $startDate, $DaysMMod4False );
-	}
+		$sum = (real)$sum + ((int)$sum + (int)$sumAdd) * (int)$DaysY * (((int)$DaysY * (int)$percent) / 366);
+		(int)$startDate[1] = (int)$startDate[1] + 1;
+		if ((int)$startDate[1] == 13)
+		{
+			(int)$startDate[1] = 1;
+			(int)$startDate[2]++;
+		};
+		(int)$DaysY = (int)$DaysMMod4False[$startDate[1] - 1];
+	};
 	
-}
-
+};
+echo json_decode ($sum);
 ?>
--- Code/script.js
+++ Code/script.js
@@ -1,49 +1,117 @@
 window.onload = function()
 {
 	
-	var MAndY = $(#MonthOrYear).options.selectedIndex;
-	function ChangeMY (MAndY)
-	{
-		MAndY = $(#MonthOrYear).options.selectedIndex;
-	}
-	$(#MonthOrYear).onchange = ChangeMY (MAndY);
+		$("#form").validate
+	({
+		rules:
+		{
+			startDate:
+			{
+				required: true,
+				date: true,
+			},
+			term:
+			{
+				required: true,
+				digits: true,
+				min: 1,
+				max: 60,
+			},
+			sum:
+			{
+				required: true,
+				digits: true,
+				min: 1000,
+				max: 3000000,
+			},
+			percent:
+			{
+				required: true,
+				digits: true,
+				min: 3,
+				max: 100,
+			},
+			sumAdd:
+			{
+				required: true,
+				digits: true,
+				min: 0,
+				max: 3000000,
+			},
+		},
+		messages:
+		{
+			startDate: "Введите корректную дату",
+			term: "Срок может быть только в районе от 1 до 60 месяцов",
+			sum: "Вклад от 1 000р. до 3 000 000р.",
+			percent: "не меньше 3% и не более 100%",
+			sumAdd: "от 0р. до 3 000 000р.",
+		},
+	});
 	
-	$(#NextPayments).onclick = function
+	var MAndY = $("#MonthOrYear").val();
+	
+	$("#MonthOrYear").change ( function()
+	{
+		MAndY = $("#MonthOrYear").val();
+	});
+	
+	$("#NextPayments").click ( function ()
 	{ //Если ежемесячного пополнения нет - передавать 0
 		
-		if ( $(#NextPayments).checked )
+		if ( $("#NextPayments").is(":checked") )
 		{
-			$(#sumAdd).style.visibility = "visible";
-			$(#sumAdd).value = "";
+			$("#sumAdd").css("visibility", "visible");
+			$("#sumAdd").val("");
 		}
 		else
 		{
-			$(#sumAdd).style.visibility = "hidden";
-			$(#sumAdd).value = "0";
+			$("#sumAdd").css("visibility", "hidden");
+			$("#sumAdd").val("0");
 		}
 		
-	}
+	});
 	
-	$(#summit).onclick = function
+	$("#submit").click( function ()
 	{
-		// инициализация
-		var startDate = $(#startDate).value;
-		var sum = $(#sum).value;
-		var term = $(#term).value * MAndY ;
-		var percet = $(#percent).value;
-		var sumAdd = $(#sumAdd).value;
 		
 		$.ajax
 		({
-			type: "Post",
 			url: "calc.php",
+			type: "POST",
 			dataType: "JSON",
-			data: {startDate, sum, term, percent, sumAdd},
+			data:
+			{
+				startDate: $("#startDate").val(),
+				sum: $("#sum").val(),
+				term: $("#term").val() * MAndY,
+				percent:$("#percent").val(),
+				sumAdd: $("#sumAdd").val()
+			},
 			success: function(data)
 			{
-				$('#results').html("₽ " + data.toLocaleString('ru'));
+				$("#results").html("₽ " + data.toLocaleString("ru"));
+			},
+			error: function (jqXHR, exception)
+			{
+				var msg = '';
+				if (jqXHR.status === 0)
+				{ msg = 'Not connect.\n Verify Network.'; } 
+				else if (jqXHR.status == 404)
+				{ msg = 'Requested page not found. [404]'; }
+				else if (jqXHR.status == 500)
+				{ msg = 'Internal Server Error [500].'; }
+				else if (exception === 'parsererror')
+				{ msg = 'Requested JSON parse failed.'; } 
+				else if (exception === 'timeout')
+				{ msg = 'Time out error.'; }
+				else if (exception === 'abort')
+				{ msg = 'Ajax request aborted.';}
+				else
+				{ msg = 'Uncaught Error.\n' + jqXHR.responseText;}
+				alert(msg);
 			}
-		})
-	}
+		});
+	});
 	
 }
